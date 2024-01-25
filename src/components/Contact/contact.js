66202dc81e823a9979d0ea36d51188f89fa47546
Pam <pwillisop@yahoo.com>
2024-01-25 14:17:56 -0500

    added linkedin and github logo to contact section

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -3,14 +3,14 @@ import Walmart from '../../assets/walmart.png';
 import Adobe from '../../assets/adobe.png';
 import Microsoft from '../../assets/microsoft.png';
 import Facebook from '../../assets/facebook.png';
-import facebookIcon from '../../assets/facebook-icon.png';
-import twitterIcon from '../../assets/twitter.png';
-import youtubeIcon from '../../assets/youtube.png';
-import instagramIcon from '../../assets/instagram.png';
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+    const linked = "https://www.linkedin.com/in/pam-willis-66a96a25";
+    const github = "https://github.com/PamWillis";
+
     const form = useRef();
 
     const sendEmail = (e) => {
@@ -25,22 +25,22 @@ const Contact = () => {
                 console.log(error.text);
             });
     };
-    
+
     return (
         <div id='contactPage'>
-            <div id="clients">
+            {/* <div id="clients">
                 <h1 className="contactPageTitle">My clients</h1>
                 <span className="clientDesc">
-                    I have had the opportunity to work with a diverse group of companies. 
+                    I have had the opportunity to work with a diverse group of companies.
                     Some of the notable companies I have worked with includes
                 </span>
                 <div className="clientImgs">
                     <img src={Walmart} alt="Client" className="clientImg" />
                     <img src={Adobe} alt="Client" className="clientImg" />
                     <img src={Microsoft} alt="Client" className="clientImg" />
-                    <img src={Facebook} alt="Client" className="clientImg" />                   
+                    <img src={Facebook} alt="Client" className="clientImg" />
                 </div>
-            </div>
+            </div> */}
             <div id="contact">
                 <h1 className="contactPageTitle">Contact Me</h1>
                 <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
@@ -50,10 +50,11 @@ const Contact = () => {
                     <textarea name="message" placeholder='Your Message' rows={5} className='msg' ></textarea>
                     <button type="submit" value="Send" className='submitBtn'>Submit</button>
                     <div className="links">
-                        <img src={facebookIcon} alt="Facebook" className="link" />
-                        <img src={twitterIcon} alt="Twitter" className="link" />
-                        <img src={youtubeIcon} alt="YouTube" className="link" />
-                        <img src={instagramIcon} alt="Instagram" className="link" />
+                        <a href={linked} target="_blank" rel="noopener noreferrer">
+                            <i className="fa fa-linkedin icon"></i></a>
+                        <a href={github} target="_blank" rel="noopener noreferrer">
+                            <i className="fa fa-github icon"></i>
+                        </a>
                     </div>
                 </form>
             </div>
