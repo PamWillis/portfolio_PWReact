83266ddd7e95619aba8797803cd102fea4f47d0e
Pam <pwillisop@yahoo.com>
2024-01-30 11:34:53 -0500

    optimized intro and skills page

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -11,20 +11,23 @@ const Intro = () => {
 
     return (
         <section id="intro">
-             <img src={Person} alt="me" className="person" />
             <div className="introContent">
-                <span className="hello">Hello,</span>
-                <span className="introText">I'm <span className="introName">Pam Willis</span> <br />Software Engineer</span>
-                <p className="introPara">I am a junior software engineer with experience in creating<br />visually appealing and user-friendly websites.</p>
-                <button className="btn" onClick={scrollToContact}>
-                    <img src={btnImg} alt="Hire" className='btnImg'/> Hire Me
-                </button>
+                <div className="personContainer">
+                    <img src={Person} alt="me" className="person" />
+                </div>
+                <div className="textContainer">
+                    <span className="hello">Hello,</span>
+                    <span className="introText">I'm <span className="introName">Pam Willis</span> <br />Software Engineer</span>
+                    <p className="introPara">I am a junior software engineer with experience in creating<br />visually appealing and user-friendly websites.</p>
+                    <button className="btn" onClick={scrollToContact}>
+                        <img src={btnImg} alt="Hire" className='btnImg'/> Hire Me
+                    </button>
+                </div>
             </div>
-           
             <img src={bg} alt="Profile" className="bg" />
-        
         </section>
     );
 }
+
 
 export default Intro;
--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -15,63 +15,64 @@ import SQL from '../../assets/SQL_logo.png';
 const Skills = () => {
     return (
         <section id='skills'>
-            <span className="skillTitle">The Pursuit</span>
+            <span className="title">The Pursuit</span>
             <div className='flex'>
-                <span className="skillDesc left-justify">I transitioned from a <strong>Graphic Artist</strong> in flexible packaging to Full-Stack web development, gaining expertise in <strong>HTML, CSS, JavaScript, React, Node.js, Express, MongoDB, and GraphQL</strong> through intensive training at the University of North Carolina Bootcamp. This journey not only enriched my technical skills but also instilled a collaborative mindset. The program emphasized the significance of teamwork in tackling the intricacies of full-stack projects.</span>
-                <span className="skillDesc left-justify">Prepared to contribute my skills and learn from others, I am well-equipped to thrive in collaborative work settings. My unique blend of technical proficiency and a problem-solving mindset, coupled with a commitment to continuous learning, positions me to take on diverse challenges in the ever-evolving field of web development.</span>
+                <span className="intro left-justify">I transitioned from a <strong>Graphic Artist</strong> in flexible packaging to Full-Stack web development, gaining expertise in <strong>HTML, CSS, JavaScript, React, Node.js, Express, MongoDB, and GraphQL</strong> through intensive training at the University of North Carolina Bootcamp. This journey not only enriched my technical skills but also instilled a collaborative mindset. The program emphasized the significance of teamwork in tackling the intricacies of full-stack projects.</span>
+                <span className="intro left-justify">Prepared to contribute my skills and learn from others, I am well-equipped to thrive in collaborative work settings. My unique blend of technical proficiency and a problem-solving mindset, coupled with a commitment to continuous learning, positions me to take on diverse challenges in the ever-evolving field of web development.</span>
             </div>
 
-            <div className="skillBars">
+            <div className="skillContainer">
                 <div className="skillBar">
-                    <img src={Javascript} alt="Javascript" className="skillBarImg" />
+                    <img src={Javascript} alt="Javascript" className="skillBarImg floatLeft" />
                     <div className="skillBarText left-justify">
                         <h2>Javascript</h2>
                         <p>This is a demo text, you can write your own content here.</p>
                     </div>
                 </div>
-                <div className="skillBar flex">
-                    <img src={ReactLogo} alt="React" className="skillBarImg" />
+                <div className="skillBar">
+                    <img src={ReactLogo} alt="React" className="skillBarImg floatLeft" />
                     <div className="skillBarText left-justify">
                         <h2>React</h2>
                         <p>This portfolio is created with react to make a dynamic site.</p>
                     </div>
                 </div>
-                <div className="skillBar flex">
-                    <div className="skillBarText left-justify">
+                <div className="skillBar">
+                    <div className="skillBarTextNoIcon">
                         <h2>HTML & CSS</h2>
                         <p>The basis of front-end developement. CSS is great for overall control of styling.</p>
                     </div>
                 </div>
-                <div className="skillBar flex">
-                    <div className='vertical_images'>
+                <div className="skillBar">
+                    <div className='vertical_images floatLeft'>
                     <img src={Tailwinds} alt="Tailwinds" className="skillBarImg" />
                     <img src={Bootstrap} alt="Bootstrap" className="skillBarImg" />
                     </div>
-                    <div className="skillBarText left-justify ml2">
+                    <div className="skillBarText left-justify">
                         <h2>Tailwinds & Bootstrap</h2>
                         <p>Used for components and single purpose styles, great for quick easy changes in front-end developement.</p>
                     </div>
                 </div>
-                <div className="skillBar flex">
-                    <div className='vertical_images'>
+                <div className="skillBar">
+                    <div className='vertical_images floatLeft'>
                     <img src={SQL} alt="Tailwinds" className="skillBarImg" />
                     <img src={Mongo} alt="Bootstrap" className="skillBarImg" />
                     </div>
-                    <div className="skillBarText left-justify ml2">
+                    <div className="skillBarText left-justify">
                         <h2>SQL & MongoDB</h2>
                         <p>Two Database programs used for backend developement.</p>
                     </div>
                 </div>
-                <div className="skillBar left-justify">
-                    <div>
+                <div className="skillBar">
+                    <div className='justify_content'>
                         <img src={Illustrator} alt="Illustrator" className="skillBarImg mt5" />
                         <img src={Photoshop} alt="Illustrator" className="skillBarImg mt5" />
                         <img src={InDesign} alt="InDesign" className="skillBarImg mt5" />
-                        <div className="skillBarText left-justify">
+                        </div>
+                        <div className="skillBarTextNoIcon">
                         <h2 className='mt2'>Adobe Creative Suite</h2>
                         <p>Illustrator, Photoshop and InDesign.</p>
                         </div>
-                    </div>
+                    
                 </div>
             </div>
         </section>
